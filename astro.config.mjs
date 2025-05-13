6670a83151b5fbc45332d995b7ac07302f29690f
David Cortés <cortestoledanodavid@gmail.com>
2025-05-13 12:02:03 +0200

    add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 import vue from '@astrojs/vue';
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,5 +20,15 @@ export default defineConfig({
     sitemap(),
     partytown(),
     vue({appEntrypoint: '/src/pages/_app'}),
+    sentry({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sourceMapsUploadOptions: {
+        project: "website",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
 });
